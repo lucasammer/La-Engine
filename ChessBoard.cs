d83cd04458f977eb4288dcf97fbf8e1d60487062
LucasAmmer <lucas@ammer.nl>
2023-03-20 13:12:57 +0100

    changed way of loading pieces

--- ChessBoard.cs
+++ ChessBoard.cs
@@ -52,6 +52,71 @@ namespace La_engine_vt3
         public void SetupBoard(string fen)
         {
             string[] firstSplit = fen.Split('/');
+            int j = 0;
+
+            for(int i = 0; i< 8; ++i)
+            {
+                for(int p = 0; p < 8; ++p)
+                {
+                    try
+                    {
+                        char c = firstSplit[i][p];
+
+                        if (char.IsDigit(c))
+                        {
+                            board[i, j] = new Piece(Piece.types.None, Piece.Color.BLACK);
+                            j += (int)Char.GetNumericValue(c);
+                        }
+                        else
+                        {
+                            switch (c)
+                            {
+                                case ('P'):
+                                    board[i, j] = new Piece(Piece.types.Pawn, Piece.Color.WHITE);
+                                    break;
+                                case ('p'):
+                                    board[i, j] = new Piece(Piece.types.Pawn, Piece.Color.BLACK);
+                                    break;
+                                case ('N'):
+                                    board[i, j] = new Piece(Piece.types.Knight, Piece.Color.WHITE);
+                                    break;
+                                case ('n'):
+                                    board[i, j] = new Piece(Piece.types.Knight, Piece.Color.BLACK);
+                                    break;
+                                case ('R'):
+                                    board[i, j] = new Piece(Piece.types.Rook, Piece.Color.WHITE);
+                                    break;
+                                case ('r'):
+                                    board[i, j] = new Piece(Piece.types.Rook, Piece.Color.BLACK);
+                                    break;
+                                case ('B'):
+                                    board[i, j] = new Piece(Piece.types.Bishop, Piece.Color.WHITE);
+                                    break;
+                                case ('b'):
+                                    board[i, j] = new Piece(Piece.types.Bishop, Piece.Color.BLACK);
+                                    break;
+                                case ('Q'):
+                                    board[i, j] = new Piece(Piece.types.Queen, Piece.Color.WHITE);
+                                    break;
+                                case ('q'):
+                                    board[i, j] = new Piece(Piece.types.Queen, Piece.Color.BLACK);
+                                    break;
+                                case ('K'):
+                                    board[i, j] = new Piece(Piece.types.King, Piece.Color.WHITE);
+                                    break;
+                                case ('k'):
+                                    board[i, j] = new Piece(Piece.types.King, Piece.Color.BLACK);
+                                    break;
+                            }
+                            ++j;
+                        }
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
             // for(int i=0;i<8;i++)
             // {
             //     int b = 0;
